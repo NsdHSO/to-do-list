bc423240df5a3dbb45fd8a4dd2a2fbe62dbe2902
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-12-21 15:51:27 +0200

    feat: added not important folder and create server

---
+++ starts.cjs
@@ -0,0 +1,15 @@
+const fastify = require('fastify')
+
+const serverOptions = {
+    logger: true
+}
+const app = fastify(serverOptions)
+
+app.listen(
+    {
+        port: 8080,
+        host: '0.0.0.0',
+    }
+).then((address) => {
+    console.log(`Server started at ${address}`)
+})
