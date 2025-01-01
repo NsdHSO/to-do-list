5e8ce226585b601c62a48c0a6e1ddab1a4053c0f
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2025-01-01 14:51:03 +0200

    feat: learning to handle 404 route

--- server.cjs
+++ server.cjs
@@ -1,47 +1,56 @@
 const Fastify = require('fastify')
 const userRouter = require('./routes/users-router.cjs')
+const fp = require('fastify-plugin')
 
-const app = Fastify({
-    logger: {
-        level: 'debug',
-        transports: {
-            target: 'pino-pretty'
-        }
-    },
-})
-app.decorate('users',
-    [
-        {name: 'sam', age: 23},
-        {name: 'sam', age: 23},
-        {name: 'Ian', age: 27},
-        {name: 'And', age: 22},
-    ])
- function options (parent){
-    return ({
-        prefix: 'V1',
-        myPlugin: {
-            firstElement: parent.mySpecialProp,
-        }
+async function boot() {
+
+    const app = Fastify({
+        logger: {
+            level: 'debug',
+            transports: {
+                target: 'pino-pretty'
+            }
+        },
+    })
+    app.decorate('users',
+        [
+            {name: 'sam', age: 23},
+            {name: 'sam', age: 23},
+            {name: 'Ian', age: 27},
+            {name: 'And', age: 22},
+        ])
+
+    function options(parent) {
+        return ({
+            prefix: parent.version_prefix,
+            myPlugin: {
+                firstElement: parent.mySpecialProp,
+            }
+        })
+    }
+
+    app.addHook('onRequest', (request, reply, done) => {
+        request.log.info('request received I a 404 HTTP request...')
+        request.is404;
+
+        done()
+    })
+    app.register((instance, opts) => {
+        instance.setNotFoundHandler((err, reply) => {
+            reply.send('Not found').status(404)
+
+        })
+    }, {prefix: '/iancu'})
+    app.register
+    await app.ready()
+
+    await app.listen({
+        port: 8080,
+        host: '0.0.0.0',
+    }).then((_) => {
+        app.log.info('server started')
+        console.log(app.printRoutes())
     })
 }
-app.register(userRouter, {prefix: 'v1'})
-app.decorate('mySpecialProp', 'pino-pretty')
-app.register(async function userRouterV2(fastify, options) {
-    fastify.delete('/users/:name', (request, reply) => {
-        const userFind = fastify.users.findIndex(user => user.name === request.params.name)
-        fastify.users.splice(userFind, 1)
-        reply.send(fastify.users)
-    })
-},
-    {prefix:'v2'}
-)
-app.register(async function logConsole(fastify, options) {
-    console.log(options.myPlugin.firstElement)
-}, options)
-app.listen({
-    port: 8080,
-    host: '0.0.0.0',
-}).then((_) => {
-    app.log.info('server started')
-    console.log(app.printRoutes())
-})
+
+boot()
